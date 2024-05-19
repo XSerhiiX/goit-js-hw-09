140e39eb71edb841cd8bf27ee13b1a29f36b7e95
XSerhiiX <96480425+XSerhiiX@users.noreply.github.com>
2024-05-19 06:34:43 +0000

    Deploying to gh-pages from @ XSerhiiX/goit-js-hw-09@000af5d40203d7f10eec0bef0759d12433e80a53 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,10 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as o}from"./assets/vendor-10cb7c31.js";console.log(o);
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as l}from"./assets/vendor-10cb7c31.js";console.log(l);const i=images.map(({preview:e,original:t,description:r})=>`
+    <li class="gallery-item">
+      <a class="gallery-link" href="${t}">
+        <img
+          class="gallery-image"
+          src="${e}"
+          data-source="${t}"
+          alt="${r}"
+        /></a></li>`).join(""),o=e=>{e.preventDefault(),e.target.nodeName==="IMG"&&basicLightbox.create(`<img src="${e.target.getAttribute("data-source")}" alt="${e.target.getAttribute("alt")}"/>`).show()},a=document.querySelector(".gallery");a.insertAdjacentHTML("beforeend",i);a.addEventListener("click",o);
 //# sourceMappingURL=commonHelpers.js.map
