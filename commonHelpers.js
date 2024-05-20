de736ee7cd575bf5defc0d6d6eaf7582f7ce7e68
XSerhiiX <96480425+XSerhiiX@users.noreply.github.com>
2024-05-20 06:32:56 +0000

    Deploying to gh-pages from @ XSerhiiX/goit-js-hw-09@88e5e1eb31ad1768a94bbdddb04cd4434055c247 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,10 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as l}from"./assets/vendor-10cb7c31.js";console.log(l);const i=images.map(({preview:e,original:t,description:r})=>`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as o}from"./assets/vendor-10cb7c31.js";console.log(o);const r=document.querySelector(".gallery"),s=images.map(({preview:e,original:a,description:l})=>`
     <li class="gallery-item">
-      <a class="gallery-link" href="${t}">
+      <a class="gallery-link" href="${a}">
         <img
           class="gallery-image"
           src="${e}"
-          data-source="${t}"
-          alt="${r}"
-        /></a></li>`).join(""),o=e=>{e.preventDefault(),e.target.nodeName==="IMG"&&basicLightbox.create(`<img src="${e.target.getAttribute("data-source")}" alt="${e.target.getAttribute("alt")}"/>`).show()},a=document.querySelector(".gallery");a.insertAdjacentHTML("beforeend",i);a.addEventListener("click",o);
+          data-source="${a}"
+          alt="${l}"
+        /></a></li>`).join("");let t;const i=e=>{e.preventDefault(),e.target.nodeName==="IMG"&&(t=basicLightbox.create(`<img src="${e.target.getAttribute("data-source")}" alt="${e.target.getAttribute("alt")}"/>`),t.show())},c=e=>{e.code==="Escape"&&t.close()};r.insertAdjacentHTML("beforeend",s);r.addEventListener("click",i);document.addEventListener("keydown",c);
 //# sourceMappingURL=commonHelpers.js.map
